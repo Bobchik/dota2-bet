bd3ab3d61d030db58053cf6cda5f58ee08904481
Bobchik <workspace1936@gmail.com>
2019-01-27 13:52:31 +0000

    fix check date in check_report

--- app/Report.php
+++ app/Report.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Report extends Model
 {
@@ -14,8 +15,9 @@ class Report extends Model
     public static function check_reporter($reporter_id, $player)
     {
         $reporter = Report::where('source_id', $reporter_id)->get();
+        $date = Carbon::now();
         foreach ($reporter as $user) {
-            if ($user->destination_id == $player) {
+            if ($user->destination_id == $player && $date->diffInHours($user->created_at) < 168) {
                 return response('User already reported', '500');
                 break;
             }
